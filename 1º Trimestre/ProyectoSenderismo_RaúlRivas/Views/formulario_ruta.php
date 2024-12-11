2d2c71af0b10e9a63be37dc7d6cfca773d921976
Raúl Rivas <raulrivasortega94@gmail.com>
2024-12-11 20:55:10 +0100

    commit

--- 1º Trimestre/ProyectoSenderismo_RaúlRivas/Views/formulario_ruta.php
+++
@@ -1,5 +0,0 @@
-<form method="post" action="/ruta/nueva">
-    <label for="titulo">Título:</label>
-    <input type="text" name="titulo" id="titulo" required>
-    <button type="submit">Añadir</button>
-</form>
--- 1º Trimestre/ProyectoSenderismo_RaúlRivas/Views/lista_rutas.php
+++
@@ -1,6 +0,0 @@
-<h1>Lista de Rutas</h1>
-<ul>
-    <?php foreach ($rutas as $ruta): ?>
-        <li><?= $ruta['titulo'] ?> - <?= $ruta['distancia_km'] ?> km</li>
-    <?php endforeach; ?>
-</ul>
