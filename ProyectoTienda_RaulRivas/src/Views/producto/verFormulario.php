f1607f32d5531a59d9b9b7a2947c15cd42a9a64f
RaulRivas04 <raulrivasortega94@gmail.com>
2025-02-11 00:34:52 +0100

    Update verFormulario.php

--- ProyectoTienda_RaulRivas/src/Views/producto/verFormulario.php
+++ ProyectoTienda_RaulRivas/src/Views/producto/verFormulario.php
@@ -5,7 +5,7 @@
     <title>Ver Formulario</title>
 </head>
 <body>
-    <h1>Formulario o Productos</h1>
+    <h1>Formulario Productos</h1>
     <ul>
         <?php foreach ($productos as $producto): ?>
             <li><?= htmlspecialchars($producto['nombre']) ?></li>
